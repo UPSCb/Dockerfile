4edc092108cfefd13f5e6cb4f170ef541f8f68ea
nicolasDelhomme <nicolas.delhomme@umu.se>
2023-10-21 08:45:02 +0000

    BUWS2301

--- delhomme/upscb-rstudio-sequencing/R-install-packages.R
+++ delhomme/upscb-rstudio-sequencing/R-install-packages.R
@@ -2,7 +2,7 @@ rlib='/home/training/.r-library'
 
 install.packages("BiocManager",repos="https://ftp.acc.umu.se/mirror/CRAN/")
 BiocManager::install(
-	pkgs=scan(what="character","R-package-list.txt"),
+	pkgs=scan(what="character","/home/training/R-package-list.txt"),
 	ask = FALSE,
 	update=TRUE,
 	Ncpus=8)
